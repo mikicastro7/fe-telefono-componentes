952af4577741c4cebaf9890caaf85eab21a1a19a
Miquel Castro <castro-miquel@hotmail.com>
2021-03-11 20:58:05 +0100

    Borro la funcion tipo de boton y hago un ternario para hacer un check de la clase

--- src/Componentes/Botones/Boton/Boton.jsx
+++ src/Componentes/Botones/Boton/Boton.jsx
@@ -3,16 +3,9 @@ import React from "react";
 const Boton = (props) => {
   const { tipo, elementoMostrar, llamando, funcionOnClick } = props;
 
-  const tipoDeBotonCrear = () => {
-    if (tipo === "numero") {
-      return <button disabled={llamando} onClick={() => funcionOnClick(elementoMostrar)}>{elementoMostrar}</button>;
-    } else {
-      return <button className="big" disabled={llamando} onClick={() => funcionOnClick(elementoMostrar)}>{elementoMostrar}</button>;
-    }
-  };
   return (
     <div>
-      {tipoDeBotonCrear()}
+      <button className={tipo === "numero" ? "" : "big"} disabled={llamando} onClick={() => funcionOnClick(elementoMostrar)}>{elementoMostrar}</button>
     </div>
   );
 };
